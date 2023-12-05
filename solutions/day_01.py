f1f447b10a615a141ae71d9639383c1705a6b8b5
Max Rosenthal <max.rosenthal@certn.co>
2023-12-04 20:46:36 -0800

    cleaned up solution file

--- solutions/day_01.py
+++ solutions/day_01.py
@@ -1,71 +1,51 @@
-import json
-import ast
+from helper_methods import get_lines_from_file
+from constants.day_01_constants import digits_and_string_numbers, string_to_num_dict, digits_only
 
 
-def get_first_number(line):
-    numbers = ["0","1","2","3","4","5","6","7","8","9"]
+def get_number(line, reversed=False):
+    if reversed:
+        line = line[::-1]
+
     for item in line:
-        if item in numbers:
-            print(item)
+        if item in digits_only:
             return item
 
-def get_last_number(line):
-    numbers = ["0","1","2","3","4","5","6","7","8","9"]
-    reversed_line = line[::-1]
-
-    for item in reversed_line:
-        if item in numbers:
-            print(item)
-            return item
-
-def do_the_thing_1():
-    f = open("aoc_day_01_data.py","r")
-    lines = f.readlines()
-    lines = [line[:-1] for line in lines]
-    sum = 0
-    for line in lines[0:1]:
-        print(type(line))
-        print(line)
-        first = get_first_number(line)
-        print(first)
-        last = get_last_number(line)
-        num = ((int(first) * 10) + int(last))
-        print(num)
-        sum += num
-
-    print(sum)
-
-def do_the_thing_2():
-    f = open("aoc_day_01_data.py","r")
-    lines = f.readlines()
-    lines = [line[:-1] for line in lines]
-    sum = 0
-    all_nums = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "0","1","2","3","4","5","6","7","8","9"]
-    string_to_num_dict = {"one": 1, "two": 2, "three": 3, "four": 4, "five": 5, "six": 6, "seven": 7, "eight": 8, "nine": 9, "0": 0,"1": 1,"2": 2,"3": 3,"4": 4,"5": 5,"6": 6,"7": 7,"8": 8,"9": 9}
+def solve_day_01_problem_1():
+    lines = get_lines_from_file("data/data_01")
+    total_sum = 0
     for line in lines:
-        # print(line)
+        first = get_number(line)
+        last = get_number(line, reversed=True)
+        total_sum += (int(first) * 10) + int(last)
+
+    print("day 1, problem 1 solution: ", total_sum)
+    return total_sum
+
+def solve_day_01_problem_2():
+    lines = get_lines_from_file("data/data_01")
+
+    total_sum = 0
+
+    for line in lines:
         valid_lowest_digits = {}
         valid_highest_digits = {}
 
-        for string in all_nums:
+        for string in digits_and_string_numbers:
             lowest_index = line.find(string)
             highest_index = line.rfind(string)
-            if lowest_index != -1:
+            if lowest_index != -1: # ignore numbers not present in string
                 valid_lowest_digits[string] = lowest_index
 
             if highest_index != -1:
                 valid_highest_digits[string] = highest_index
 
+        # gets the number with the starting index in the lowest spot
         first_num = min(valid_lowest_digits, key=valid_lowest_digits.get)
+        # gets the number with the starting index in the highest spot
         last_num = max(valid_highest_digits, key=valid_highest_digits.get)
-        # print(valid_lowest_digits)
-        # print(valid_highest_digits)
-        # print(first_num, string_to_num_dict.get(first_num))
-        # print(last_num, string_to_num_dict.get(last_num))\
-        num = string_to_num_dict.get(first_num) * 10 + string_to_num_dict.get(last_num)
-        sum += num
-    print(sum)
 
-if __name__ == "__main__":
-    # do_the_thing_1()
-    do_the_thing_2()
+        num = string_to_num_dict.get(first_num) * 10 + string_to_num_dict.get(last_num)
+        total_sum += num
+
+    print("day 1, problem 2 solution: ", total_sum)
+    return total_sum
